17ed283f8d3f850d521fe77a92691281a74e5288
Matt Danskine <danskinem@lanecc.edu>
2014-08-05 10:48:52 -0700

    Edits to documents section of php file
    Decided that all the same info is in the readme, so why duplicate it.

--- 1-click-wp.php
+++ 1-click-wp.php
@@ -1,37 +1,8 @@
 <?php
 /*///////////////////////////////////////////////////////////////////////////
 	
-	One-Click WordPress Installer 
+	One-Click WordPress Installer (v1.2.1)  
 	By Fotan (www.fotan.net)
-	
-	v1.2 (4/29/2014)
-		- Backed out of deleting the license.txt and readme.html files
-		- Added some security "stuff" to htaccess
-		- Added DISALLOW_FILE_EDIT to wp-config.php so file editing from Dashboard is disabled
-		- Added Simple Custom CSS to install list
-		- Added Limit Login Attempts plugin to install list
-		- Added an uploads folder to wp-content
-	v1.1 (4/4/2014)
-		- Setup screen now includes a list of the plugins I always seem to want to 
-		  install with check boxes.  Just check the box and the latest version of the 
-		  plugin is downloaded and unzipped in the wp-content/plugins folder.
-		- To add more plugins to download and unzip, just copy and paste one of hte
-		  ones already there and change the value and text after the input.
-		- After unzipping the plugin file, the .zip file is deleted from the server
-		- Cleaned up the CSS in the form a little.
-	v1.0
-		- Downloads the latest, stable version of WordPress
-		- Displays a form to fill in DB Host, DB User, DB Password
-		- Randomly creates the SALT fields (hidden from view)
-		- Randomly creates the DB Prefix, but lets you change it if you want
-		- Displays a form to fill in FTP Host, FTP User, FTP Password, SSL?  
-		  If properly filled in, you will be able to use one touch installs and updates
-		- Displays a form to set a Memory Limit
-		- Deletes the tar ball of the install package
-		- Deletes license.txt, because it's silly to have
-		- Deletes readme.html, because it's silly to have
-		- Creates .htaccess file and makes it writable to the user (664)
-		- Forwards to instal.php
 	
 	
 	
